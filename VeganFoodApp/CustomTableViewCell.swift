058392323e7bbda60b887069ca72ab0e82d41b5d
Kamnev Vladimir <Camnev.vov@yandex.ru>
2020-11-19 02:36:53 +0300

    Custom Table View Cell

---
+++ VeganFoodApp/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  VeganFoodApp
+//
+//  Created by Tsar on 19.11.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
